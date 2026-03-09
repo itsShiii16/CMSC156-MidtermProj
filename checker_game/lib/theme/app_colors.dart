bd807e5b21515be54323c70c03a88a5db022cc5d
Sherwin Paul Sabandal <slsabandal@up.edu.ph>
2026-03-09 10:14:00 +0800

    initialize: Project Structure and Color Theme

---
+++ checker_game/lib/theme/app_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color screenFrame = Color(0xFF1A1A1A);
+  static const Color cream = Color(0xFFF3E6D7);
+  static const Color beige = Color(0xFFE2CDB7);
+  static const Color boardLight = Color(0xFFE7D4BF);
+  static const Color boardDark = Color(0xFF996523);
+  static const Color navyButton = Color(0xFF0B1128);
+  static const Color whitePiece = Color(0xFFF6F1EB);
+  static const Color blackPiece = Color(0xFF111111);
+  static const Color softGray = Color(0xFF7B7B7B);
+  static const Color gold = Color(0xFFF3C623);
+  static const Color redNotif = Color(0xFFE53935);
+  static const Color greenOn = Color(0xFF8BC34A);
+  static const Color redOff = Color(0xFF8B1E2D);
+}
